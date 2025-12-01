6aff07a574569b15818a885acfd646bc92d269e1
Brigham Brown <me@brighambrown.com>
2025-12-01 13:14:48 -0800

    v0.4.0+1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.4.0+1
+
+* Export the code this time.
+
 # 0.4.0
 
 * Fork from `pb_dtos`, removing unrelated code.
---
+++ lib/pb_obtain.dart
@@ -0,0 +1,6 @@
+export 'src/tools/executable_config.dart';
+export 'src/tools/launch.dart';
+export 'src/tools/launch_config.dart';
+export 'src/tools/obtain.dart';
+export 'src/tools/obtain_config.dart';
+export 'src/tools/validate_exception.dart';
